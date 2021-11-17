ac3b442f7a6ee4fc08b2ec62183317625ab29e1f
Gavin Gray <graygavin111997@gmail.com>
2021-11-16 21:13:54 -0800

    Working

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var http = require('http');
 var fs = require('fs');
+var extract = require('./extract');
+var wss = require('./websockets-server');
+
+function handleError(err, res) {
+  res.writeHead(404);
+  res.end();
+}
+
 var server = http.createServer(function (req, res) {
   console.log('Responding to a request.');
-  var url = req.url;
-  var fileName = 'index.html';
-  if (url.length > 1) {
-    fileName = url.substring(1);
-  }
-  console.log(fileName);
-  fs.readFile('app/index.html', function (err, data) {
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function (err, data) {
+    if (err) {
+      handleError(err, res);
+      return;
+    }
     res.end(data);
+
   });
 });
+server.listen(3000);
